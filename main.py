2379799d7b461858eda01c2d24ebbe2ad59f7392
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-08-04 15:24:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-import requests
 from pyrogram import idle
 from pyrogram import Client as Bot
+from modules.clientbot import run
+from modules.config import API_ID, API_HASH, BOT_TOKEN
 
-from callsmusic import run
-from config import API_ID, API_HASH, BOT_TOKEN
-
-
+    
 bot = Bot(
     ":memory:",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="handlers")
+    plugins=dict(root="plugins")
 )
 
 bot.start()
